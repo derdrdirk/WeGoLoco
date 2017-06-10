a7cfec34a011aab16e70042d014e2c3116def121
Knowledge <dirkhornung91@gmail.com>
2017-06-10 20:28:24 +0200

    added Swiper

--- Tinpons/App/Swiper/SwiperViewController.swift
+++ Tinpons/App/Swiper/SwiperViewController.swift
@@ -1,36 +1,96 @@
 //
-//  SwiperViewController.swift
-//  Tinpons
+//  BackgroundAnimationViewController.swift
+//  Koloda
 //
-//  Created by Dirk Hornung on 10/6/17.
-//
+//  Created by Eugene Andreyev on 7/11/15.
+//  Copyright (c) 2015 CocoaPods. All rights reserved.
 //
 
 import UIKit
 import Koloda
+import pop
+
+private let numberOfCards: Int = 5
+private let frameAnimationSpringBounciness: CGFloat = 9
+private let frameAnimationSpringSpeed: CGFloat = 16
+private let kolodaCountOfVisibleCards = 2
+private let kolodaAlphaValueSemiTransparent: CGFloat = 0.1
 
 class SwiperViewController: UIViewController {
-
+    
+    @IBOutlet weak var kolodaView: CustomKolodaView!
+    
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        kolodaView.alphaValueSemiTransparent = kolodaAlphaValueSemiTransparent
+        kolodaView.countOfVisibleCards = kolodaCountOfVisibleCards
+        kolodaView.delegate = self
+        kolodaView.dataSource = self
+        kolodaView.animator = BackgroundKolodaAnimator(koloda: kolodaView)
+        
+        self.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: IBActions
+    @IBAction func leftButtonTapped(_ sender: Any) {
+        kolodaView?.swipe(.left)
     }
-    */
+    @IBAction func rightButtonTapped(_ sender: UIButton) {
+        kolodaView?.swipe(.right)
+    }
+    @IBAction func undoButtonTapped(_ sender: UIButton) {
+        kolodaView?.revertAction()
+    }
+}
 
+//MARK: KolodaViewDelegate
+extension SwiperViewController: KolodaViewDelegate {
+    
+    func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+        kolodaView.resetCurrentCardIndex()
+    }
+    
+    func koloda(_ koloda: KolodaView, didSelectCardAt index: Int) {
+        UIApplication.shared.openURL(URL(string: "https://yalantis.com/")!)
+    }
+    
+    func kolodaShouldApplyAppearAnimation(_ koloda: KolodaView) -> Bool {
+        return true
+    }
+    
+    func kolodaShouldMoveBackgroundCard(_ koloda: KolodaView) -> Bool {
+        return false
+    }
+    
+    func kolodaShouldTransparentizeNextCard(_ koloda: KolodaView) -> Bool {
+        return true
+    }
+    
+    func koloda(kolodaBackgroundCardAnimation koloda: KolodaView) -> POPPropertyAnimation? {
+        let animation = POPSpringAnimation(propertyNamed: kPOPViewFrame)
+        animation?.springBounciness = frameAnimationSpringBounciness
+        animation?.springSpeed = frameAnimationSpringSpeed
+        return animation
+    }
+}
+
+// MARK: KolodaViewDataSource
+extension SwiperViewController: KolodaViewDataSource {
+    
+    func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+        return .default
+    }
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return numberOfCards
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        return UIImageView(image: UIImage(named: "cards_\(index + 1)"))
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
+        return Bundle.main.loadNibNamed("CustomOverlayView", owner: self, options: nil)?[0] as? OverlayView
+    }
 }
