96464ece3800992dc50455a520cfb473b139d6b8
Knowledge <dirkhornung91@gmail.com>
2017-06-14 21:35:15 +0200

    AddProductViewController: added progress bar on image upload

--- Tinpons/App/AddProduct/AddProductViewController.swift
+++ Tinpons/App/AddProduct/AddProductViewController.swift
@@ -14,8 +14,11 @@ import AWSMobileHubHelper
 
 class AddProductViewController: FormViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     var table = TinponsTable()
     var product = Product()
+    var s3Prefix = "public/"
     
     
     struct Product {
@@ -33,6 +36,18 @@ class AddProductViewController: FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set up progress bar (right under the navigationController tob bar)
+        let navBar = self.navigationController?.navigationBar
+        let navBarHeight = navBar?.frame.height
+        let progressFrame = progressBar.frame
+        let pSetX = CGFloat(0)
+        let pSetY = CGFloat(navBarHeight!)
+        let pSetWidth = view.frame.size.width
+        let pSetHeight = progressFrame.height
+        progressBar.frame = CGRect(x: pSetX, y: pSetY, width: pSetWidth, height: pSetHeight)
+        self.navigationController?.navigationBar.addSubview(progressBar)
+        
+        // Set up Eureka form
         form +++ Section("Product")
             <<< TextRow(){ row in
                 row.title = "Name"
@@ -59,14 +74,10 @@ class AddProductViewController: FormViewController {
     
     @IBAction func save(_ sender: UIBarButtonItem) {
         print("Name: \(product.name), Image: \(product.image)")
-//        if let nameRow: TextRow = form.rowBy(tag: "name"),
-//            let name = nameRow.value {
-//            product = Product(name: name)
-//        }
-//        if let imageRow: ImageRow? = form.rowBy(tag: "image"),
-//            let image = imageRow?.value as? UIImage {
-//            product.image = image
-//        }
+        
+        let imageId = product.name!
+        let s3UploadUrl = "\(s3Prefix)\(imageId)"
+        uploadWithData(data: product.imageData!, forKey: s3UploadUrl)
         
 //        insertDataWithCompletionHandler({(errors: [NSError]?) -> Void in
 //            //self.activityIndicator.stopAnimating()
@@ -85,38 +96,38 @@ class AddProductViewController: FormViewController {
     
     // MARK: AWS Saver
     
-//    func insertDataWithCompletionHandler(_ completionHandler: @escaping (_ errors: [NSError]?) -> Void) {
-//        let objectMapper = AWSDynamoDBObjectMapper.default()
-//        var errors: [NSError] = []
-//        let group: DispatchGroup = DispatchGroup()
-//        
-//        
-//        let item: Tinpons = Tinpons()
-//        item._id = NoSQLSampleDataGenerator.randomSampleStringWithAttributeName("Id")
-//        item._name = product!.name
-//        
-//        group.enter()
-//        
-//        objectMapper.save(item, completionHandler: {(error: Error?) -> Void in
-//            if error != nil {
-//                DispatchQueue.main.async(execute: {
-//                    errors.append(error! as NSError)
-//                })
-//            }
-//            group.leave()
-//        })
-//        
-//        group.notify(queue: DispatchQueue.main, execute: {
-//            if errors.count > 0 {
-//                completionHandler(errors)
-//            }
-//            else {
-//                completionHandler(nil)
-//            }
-//        })
-//    }
+    func insertDataWithCompletionHandler(_ completionHandler: @escaping (_ errors: [NSError]?) -> Void) {
+        let objectMapper = AWSDynamoDBObjectMapper.default()
+        var errors: [NSError] = []
+        let group: DispatchGroup = DispatchGroup()
+        
+        
+        let item: Tinpons = Tinpons()
+        item._id = NoSQLSampleDataGenerator.randomSampleStringWithAttributeName("Id")
+        item._name = product.name
+        
+        group.enter()
+        
+        objectMapper.save(item, completionHandler: {(error: Error?) -> Void in
+            if error != nil {
+                DispatchQueue.main.async(execute: {
+                    errors.append(error! as NSError)
+                })
+            }
+            group.leave()
+        })
+        
+        group.notify(queue: DispatchQueue.main, execute: {
+            if errors.count > 0 {
+                completionHandler(errors)
+            }
+            else {
+                completionHandler(nil)
+            }
+        })
+    }
     
-    private func uploadWithData(data: NSData, forKey key: String) {
+    private func uploadWithData(data: Data, forKey key: String) {
         let manager = AWSUserFileManager.defaultUserFileManager()
         let localContent = manager.localContent(with: data as Data, key: key)
         localContent.uploadWithPin(
@@ -124,6 +135,7 @@ class AddProductViewController: FormViewController {
             progressBlock: {[weak self](content: AWSLocalContent, progress: Progress) -> Void in
                 guard let strongSelf = self else { return }
                 /* Show progress in UI. */
+                self?.progressBar.progress = Float(progress.fractionCompleted)
             },
             completionHandler: {[weak self](content: AWSLocalContent?, error: Error?) -> Void in
                 guard let strongSelf = self else { return }
